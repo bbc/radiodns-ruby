079f811ebe381185c5c948bfc651dd4e620495e5
Chris Lowis <chris.lowis@gmail.com>
2011-03-06 15:15:25 +0000

    construct fqdn for fm bearer

--- lib/radiodns.rb
+++ lib/radiodns.rb
@@ -7,5 +7,23 @@ module RadioDNS
       cname = resolver.getresource(fqdn, Resolv::DNS::Resource::IN::CNAME)
       cname.name.to_s
     end
+
+    def self.construct_fqdn(params)
+      bearer = params[:bearer]
+      case bearer
+        when "fm" then construct_fqdn_for_fm(params)
+      end
+    end
+
+    private
+    def self.construct_fqdn_for_fm(params)
+      raise ArgumentError if params[:ecc] && params[:country]
+      raise ArgumentError if params[:ecc].nil? && params[:country].nil?
+      [params[:freq],
+       params[:pi],
+       params[:ecc] || params[:country],
+       params[:bearer],
+       'radiodns.org'].join('.')
+    end
   end
 end
--- spec/radiodns_spec.rb
+++ spec/radiodns_spec.rb
@@ -19,4 +19,47 @@ describe "RadioDNS::Resolver" do
       assert_equal 'rdns.musicradio.com', cname
     end
   end
+
+  describe "construct_fqdn" do
+    describe "for FM/VHF bearer" do
+      it "should construct a fqdn when ecc is supplied" do
+        params = {
+          :bearer => 'fm',
+          :ecc => 'ce1',
+          :pi => 'c585',
+          :freq => '09580'
+        }
+        fqdn = RadioDNS::Resolver.construct_fqdn(params)
+        assert_equal '09580.c585.ce1.fm.radiodns.org', fqdn
+      end
+      it "should construct a fqdn when country is supplied" do
+        params = {
+          :bearer => 'fm',
+          :country => 'gb',
+          :pi => 'c585',
+          :freq => '09580'
+        }
+        fqdn = RadioDNS::Resolver.construct_fqdn(params)
+        assert_equal '09580.c585.gb.fm.radiodns.org', fqdn
+      end
+      it "should raise when country and ecc is supplied" do
+        params = {
+          :bearer => 'fm',
+          :country => 'gb',
+          :ecc => 'ce1',
+          :pi => 'c585',
+          :freq => '09580'
+        }
+        assert_raises(ArgumentError) {RadioDNS::Resolver.construct_fqdn(params)}
+      end
+      it "should raise when neither country nor ecc is supplied" do
+        params = {
+          :bearer => 'fm',
+          :pi => 'c585',
+          :freq => '09580'
+        }
+        assert_raises(ArgumentError) {RadioDNS::Resolver.construct_fqdn(params)}
+      end
+    end
+  end
 end
